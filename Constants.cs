2ee46b41cbc09c934f7b4969e43e131cefcbbec6
grimpmaster <srf4hire@hotmail.com>
2019-09-23 01:18:32 -0700

    SRF001-CodeChallenge-StretchGoals

--- Constants.cs
+++ Constants.cs
@@ -3,9 +3,10 @@
     public class Constants
     {
         public static readonly string ConsoleLine = new string('-', 80);
-        public const int StartIndexOfNumbersWithCustomDelimiter = 3;
-        public const int StartIndexOfCustomDelimiter = 2;
+        public const int NumbersWithCustomDelimiterStartIndex = 3;
+        public const int CustomDelimiterStartIndex = 2;
         public const string CustomDelimiterIdentifier = "//";
+        public const int MaxNumberLimit = 1000;
     }
 
 }
